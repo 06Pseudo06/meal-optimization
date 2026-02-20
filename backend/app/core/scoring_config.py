7bcc41d7a65a716e70212648e592584a28a7447b
Kalyani Mahajan <186454190+06Pseudo06@users.noreply.github.com>
2026-02-20 18:55:12 +0530

    Delete backend directory

--- backend/app/core/scoring_config.py
+++
@@ -1,7 +0,0 @@
-SCORING_WEIGHTS = {
-    "ingredient_match": 0.35,
-    "protein_alignment": 0.25,
-    "calorie_alignment": 0.20,
-    "goal_tag_match": 0.15,
-    "macro_density": 0.05
-}
--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .recipe import Recipe
-from .ingredient import Ingredient
-from .association import RecipeIngredient
-from .daily_log import DailyLog
-from .recommendation_log import RecommendationLog
